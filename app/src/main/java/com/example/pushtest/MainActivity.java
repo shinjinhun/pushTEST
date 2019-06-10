cbcfeba0e01b493b7e3b373a48272a10adc9444c
jhshin <mamsorea@gmail.com>
2019-06-10 10:59:53 +0900

    git start test

--- app/src/main/java/com/example/pushtest/MainActivity.java
+++ app/src/main/java/com/example/pushtest/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.pushtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 
@@ -14,7 +16,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // 단말기 토큰값 가져오기
-        String token = FirebaseInstanceId.getInstance().getToken();
+        final String token = FirebaseInstanceId.getInstance().getToken();
+
+        Button button01 = (Button) findViewById(R.id.btn01);
+
+        button01.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Log.d("jhTest",token);
+            }
+        });
+
         Log.e("jhTest",token);
         Log.d("jhTest",token);
     }
